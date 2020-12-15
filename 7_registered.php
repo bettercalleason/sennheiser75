a694cdf06eab277125d27c3fdebd99c3f9e54ff9
Eason Liu <onpica17@gmail.com>
2020-12-16 03:32:50 +0800

    取消註解 $("#birthday").datepicker()

--- 7_registered.php
+++ 7_registered.php
@@ -140,9 +140,7 @@
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script>
   $(function() {
-    // $("#birthday").datepicker();
-
-
+    $("#birthday").datepicker();
   });
 </script>
 
